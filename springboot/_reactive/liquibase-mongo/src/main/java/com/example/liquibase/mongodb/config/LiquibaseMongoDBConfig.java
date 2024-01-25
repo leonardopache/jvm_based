d87ed8f76b1a22d826430ba3e26cb79e1b4c46f2
Leonardo Marqués <leonardo.marques@mail.schwarz>
2024-01-25 10:32:21 +0100

    update some comments and readme

--- springboot/_reactive/liquibase-mongo/src/main/java/com/example/liquibase/mongodb/config/LiquibaseMongoDBConfig.java
+++ springboot/_reactive/liquibase-mongo/src/main/java/com/example/liquibase/mongodb/config/LiquibaseMongoDBConfig.java
@@ -1,16 +1,10 @@
 package com.example.liquibase.mongodb.config;
 
-import liquibase.Contexts;
-import liquibase.Liquibase;
 import liquibase.command.CommandResults;
 import liquibase.command.CommandScope;
 import liquibase.command.core.UpdateCommandStep;
-import liquibase.command.core.UpdateSqlCommandStep;
 import liquibase.command.core.helpers.DbUrlConnectionCommandStep;
-import liquibase.database.DatabaseFactory;
 import liquibase.exception.LiquibaseException;
-import liquibase.ext.mongodb.database.MongoLiquibaseDatabase;
-import liquibase.resource.ClassLoaderResourceAccessor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -29,15 +23,10 @@ public class LiquibaseMongoDBConfig {
      * Execute the update when start the application.
      */
     @Bean
-    public CommandResults liquibaseUpdate() {
-        try {
-            return new CommandScope("update")
-                    .addArgumentValue(UpdateCommandStep.CHANGELOG_FILE_ARG, changeLogFile)
-                    .addArgumentValue(DbUrlConnectionCommandStep.URL_ARG, connectionUrl)
-                    .execute();
-        } catch (LiquibaseException e) {
-            log.error("error running liquibase {}", e.getMessage());
-            throw new RuntimeException(e);
-        }
+    public CommandResults liquibaseUpdate() throws LiquibaseException {
+        return new CommandScope("update")
+                .addArgumentValue(UpdateCommandStep.CHANGELOG_FILE_ARG, changeLogFile)
+                .addArgumentValue(DbUrlConnectionCommandStep.URL_ARG, connectionUrl)
+                .execute();
     }
 }
